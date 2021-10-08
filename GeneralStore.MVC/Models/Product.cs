26bede0576b9846b987b10d3a09d6b9e1f5c455d
leilahsmith <leilah.sdev@gmail.com>
2021-10-07 21:16:11 -0300

    Add project files.

---
+++ GeneralStore.MVC/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int InventoryCount { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public Boolean IsFood { get; set; }
+    }
+}
